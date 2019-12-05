b1a51c5fbcc6667a5b07b540f4f816aa6d85a16e
Kinrezc <matt.czernik@gmail.com>
2019-12-05 14:02:01 -0500

    add abridged sdk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// Inject into import block
+import {
+  getSdkEnvironment,
+  SdkEnvironmentNames,
+  createSdk
+} from "@archanova/sdk";
+
+// Select the ethereum network
+let sdkEnv = getSdkEnvironment(SdkEnvironmentNames.Kovan); // kovan env by default
+// Create SDK instance and store locally
+const sdk = new createSdk(sdkEnv.setConfig("storageAdapter", localStorage));
+// Initialize instance
+sdk.initialize().then(console.log(sdk));
 
 function App() {
   return (
